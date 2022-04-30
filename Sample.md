0699008edf4f92bb3731e5106ad0769feadfc757
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-04-30 11:15:38 -0400

    started attempts to write the code to render the license badge. No success yet.

---
+++ Sample.md
@@ -0,0 +1,32 @@
+# Some
+## Description
+
+🔎 help me.
+
+
+## Installation
+
+💾 import
+
+## Purpose
+
+to kill me.
+
+
+## Usage
+
+!Click [here](jajaja) to view the full instructional video.
+
+## License
+
+🪪 Apache
+
+## Deployment
+
+🚀 Click [here](booooo) to view the repo.  
+🚀 Click [here](meh) to view the deployed site.
+
+## Contact
+
+Feel free to contact me with any questions at: lololol
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licBadge = ${answers.license}
+  if (licBadge == 'Apache') {
+    licBadge == `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -31,7 +36,7 @@ ${answers.purpose}
 
 ## License
 
-🪪 ${answers.license}
+🪪 ${licBadge}
 
 ## Deployment
 
@@ -46,3 +51,4 @@ Feel free to contact me with any questions at: ${answers.email}
 }
 
 module.exports = generateMarkdown;
+renderLicenseBadge();
